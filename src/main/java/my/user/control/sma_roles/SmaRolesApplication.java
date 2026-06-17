f64a342ff4a250ac6f343cc6f0edfa4429707cb6
anton <ork83bork@mail.ru>
2026-06-17 17:19:26 +0300

    init project

---
+++ src/main/java/my/user/control/sma_roles/SmaRolesApplication.java
@@ -0,0 +1,13 @@
+package my.user.control.sma_roles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmaRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmaRolesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/user/control/sma_roles/SmaRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package my.user.control.sma_roles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmaRolesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
